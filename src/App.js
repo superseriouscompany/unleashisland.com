ee94e531f5074cec88f122670d111e7f5d80a166
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-05-04 20:30:46 +0100

    Make placeholder images scroll left to right airbnb style

--- src/App.js
+++ src/App.js
@@ -1,66 +1,99 @@
 import React, { Component } from 'react';
+import Form                 from './containers/Form';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.submit       = this.submit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.state        = {
-      form: {},
-    }
-  }
-
-  handleChange(e) {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value,
-      }
-    })
-  }
-
   render() {
     return (
       <div className="App">
-        <form className="application" onSubmit={this.submit}>
-          <label>
-            Name:
-            <input name="name"
-                   type="text"
-                   value={this.state.form.name || ''}
-                   onChange={this.handleChange}
-              />
-          </label>
-
-          <label>
-            Email:
-            <input name="email"
-                   type="text"
-                   value={this.state.form.email || ''}
-                   onChange={this.handleChange}
-                  />
-          </label>
-
-          <input type="submit" value="Try it" />
-        </form>
+        <section className="section">
+          <h2>The Country</h2>
+          <p>
+            If admitted, you'll be living on a small island called Ilha do Baleal,
+            connected to the mainland by a land bridge.
+          </p>
+          <div className="scroller">
+            <div className="photos">
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <h2>The House</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div className="scroller">
+            <div className="photos">
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section">
+          <h2>The People</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <div className="scroller">
+            <div className="photos">
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+              <div className="photo">
+                <img src="https://placeholdit.imgix.net/~text?txtsize=34&txt=360%C3%97360&w=360&h=360" />
+              </div>
+            </div>
+          </div>
+        </section>
+        <Form />
       </div>
     );
-  }
-
-  submit(e) {
-    e.preventDefault()
-    fetch('https://fchyjaf26f.execute-api.eu-west-1.amazonaws.com/production', {
-      method: 'POST',
-      body: JSON.stringify(this.state.form),
-      headers: { 'Content-Type': 'application/json' },
-    }).then(function() {
-      alert('Sent.')
-      console.log('Sent.')
-    }).catch(function(err){
-      console.error(err)
-      alert(err.message || JSON.stringify(err))
-    })
   }
 }
 
---
+++ src/containers/Form.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.submit       = this.submit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.state        = {
+      form: {},
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      }
+    })
+  }
+
+  render() {
+    return (
+      <form className="application" onSubmit={this.submit}>
+        <label>
+          Name:
+          <input name="name"
+                 type="text"
+                 value={this.state.form.name || ''}
+                 onChange={this.handleChange}
+            />
+        </label>
+
+        <label>
+          Email:
+          <input name="email"
+                 type="text"
+                 value={this.state.form.email || ''}
+                 onChange={this.handleChange}
+                />
+        </label>
+
+        <input type="submit" value="Try it" />
+      </form>
+    );
+  }
+
+  submit(e) {
+    e.preventDefault()
+    fetch('https://fchyjaf26f.execute-api.eu-west-1.amazonaws.com/production', {
+      method: 'POST',
+      body: JSON.stringify(this.state.form),
+      headers: { 'Content-Type': 'application/json' },
+    }).then(function() {
+      alert('Sent.')
+      console.log('Sent.')
+    }).catch(function(err){
+      console.error(err)
+      alert(err.message || JSON.stringify(err))
+    })
+  }
+}

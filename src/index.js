1c178c78f6a7a0aa6257926e0e4c6bc1a4009cdf
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-05-04 15:59:07 +0100

    Add google analytics in prod

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React    from 'react';
 import {render} from 'react-snapshot';
-import App from './App';
+import App      from './App';
+import ReactGA  from 'react-ga'
 import './index.css';
+
+if( !window.location.href.match(/localhost/) ) {
+  ReactGA.initialize('UA-83917648-3');
+
+  document.addEventListener("DOMContentLoaded", function() {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  });
+}
 
 render(
   <App />,
